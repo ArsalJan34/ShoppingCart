bedcef9d1c1792e8ce023da76bd0ae41346875b3
shahzaib00r <rajarshahzaib99@gmail.com>
2024-05-18 18:51:45 +0500

    Replace Orange with pent and change price of shirt and price of pent in Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ public class Main {
         ShoppingCart cart = new ShoppingCart();
 
         cart.addItem(new Item("Jacket", 50.0, 3));
-        cart.addItem(new Item("Shirt", 0.59, 5));
-        cart.addItem(new Item("Orange", 1.29, 2));
+        cart.addItem(new Item("Shirt", 59.0, 5));
+        cart.addItem(new Item("pent", 55.0, 2));
 
         System.out.println("Cart after adding items:");
         cart.showCart();
